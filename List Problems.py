6ef00f0ab5a2958227bdcd56ef36377d6a9f0bbc
Anindya Sarder <134484183+Anindya-coder@users.noreply.github.com>
2026-04-12 00:39:58 +0600

    List Problems.py

--- List Problems.py
+++ List Problems.py
@@ -87,4 +87,16 @@ def findIndex (self,arr, key ):
             
         return [start, end]
 
-#
+#Given a binary array nums, return the maximum number of consecutive 1's in the array.
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = 0
+        max_count = 0
+        for num in nums:
+            if num == 1:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 0
+        return max_count
+
